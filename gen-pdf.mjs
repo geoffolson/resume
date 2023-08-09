05bb248c4b3a6c82a4a16372a88a1b9ea28009ae
Geoffrey Olson <geoffrey.olson.jr@gmail.com>
2023-08-09 00:51:42 -0700

    add build script to generate resume in pdf format

---
+++ gen-pdf.mjs
@@ -0,0 +1,32 @@
+import puppeteer from "puppeteer";
+import { readFileSync, readdirSync } from "fs";
+
+(async () => {
+  // Create a browser instance
+  const browser = await puppeteer.launch();
+
+  // Create a new page
+  const page = await browser.newPage();
+
+  const html = readFileSync("./out/index.html", "utf-8");
+
+  const cssDirPath = "./out/_next/static/css/";
+  const cssFileName = readdirSync(cssDirPath, "utf-8")[0];
+  const css = readFileSync(cssDirPath + cssFileName, "utf-8");
+
+  await page.setContent(html, { waitUntil: "domcontentloaded" });
+  await page.addStyleTag({ content: css });
+
+  //To reflect CSS used for screens instead of print
+  await page.emulateMediaType("screen");
+
+  // Downlaod the PDF
+  await page.pdf({
+    path: "GeoffreyOlsonJr.pdf",
+    margin: { top: "50px", right: "50px", bottom: "100px", left: "50px" },
+    printBackground: true,
+    format: "A4",
+  });
+
+  await browser.close();
+})();
